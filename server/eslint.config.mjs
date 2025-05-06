21def0729c1073fbafa826192a631636cf280311
jeddnuget <jeddnugget@gmail.com>
2025-05-06 14:53:44 +1000

    Adjusting ESLint rules

--- server/eslint.config.mjs
+++ server/eslint.config.mjs
@@ -1,4 +1,3 @@
-import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
@@ -6,8 +5,26 @@ import { defineConfig } from "eslint/config";
 
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
   tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+
+  // 🔧 Add custom rules here:
+  tseslint.config({
+    rules: {
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "warn", 
+      "no-var": "off",
+      "semi": ["error", "always"],
+    },
+  }),
 ]);
+
