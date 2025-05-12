f12c3da58d9ca238cc04ee9e0cf9ba437c3fbd81
jeddnuget <jeddnugget@gmail.com>
2025-05-12 12:35:00 +1000

    Adding sample data for dev

---
+++ server/data/sample_data.sql
@@ -0,0 +1,28 @@
+-- Sample Users data --
+
+INSERT INTO users(id, f_name, l_name, email, password)
+VALUES
+('11111111-1111-1111-1111-111111111111', 'Jedd', 'Nugent', 'jeddnugent@gmail.com', 'testPassword123'),
+('22222222-2222-2222-2222-222222222222', 'Bukayo', 'Saka', 'BukayoSaka@gmail.com', 'ArsenalPremWinner2026');
+
+-- Sample entry data associated with sample users --
+
+INSERT INTO entrys (
+    user_id, item_date, time_consumed, item_desc, consumed_location,
+    consumption_company, feeling_prior, feeling_post, self_talk, other_comment
+) VALUES
+('11111111-1111-1111-1111-111111111111', '2020-12-02', '15:43', 'Cheese Burger', 'In Maccas carpark',
+ 'Alone', 'Stress, uneasy, desperate', 'gross, greasy, slimey',
+ 'I am a gross little piggy for eating this', 'I got extra pickles'),
+
+('11111111-1111-1111-1111-111111111111', '2024-06-02', '04:43', 'Leg of Ham', 'Office bathroom cubical',
+ 'Pattrick', 'Sneaky, subversive, guilty', 'successful, strong, bold',
+ 'I have never seen Pattrick eat so much, it kind of intimidated me', 'Honey glaze on the ham'),
+
+('11111111-1111-1111-1111-111111111111', '2024-12-02', '07:43', '15 raw cellery sticks', 'In the dark in my Kitchen',
+ 'With my wife, also in the dark', 'glorious, free, ready', 'satisfied, fixed, whole',
+ 'I am god incarnate', 'the crunch was amazing'),
+
+('22222222-2222-2222-2222-222222222222', '2024-12-02', '07:43', '15 raw cellery sticks', 'In the dark in my Kitchen',
+ 'With my wife, also in the dark', 'glorious, free, ready', 'satisfied, fixed, whole',
+ 'I am god incarnate', 'the crunch was amazing');
