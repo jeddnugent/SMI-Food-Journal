f60b34a7c20b95aa89b1ab234d0729f2e76d1779
jeddnuget <jeddnugget@gmail.com>
2025-05-12 20:17:54 +1000

    Adding fetchUserData method
    Runs on render to get all current user entry data

--- client/src/Pages/App.tsx
+++ client/src/Pages/App.tsx
@@ -1,55 +1,64 @@
-import {useState} from 'react';
+import { useState, useEffect } from 'react';
 import "../styles/App.css"
 import Header from '../components/Header';
 import NewEntryForm from '../components/NewEntryForm';
 import JournalEntryDisplayBlock from '../components/JournalEntryDisplayBlock';
+import { getAllUserEntrys } from '../api/entrys';
+import Entry from '../interfaces/Entry';
 
 function App() {
 
-	type JournalEntry = {
-		itemDate: string;
-		timeConsumed: string;
-		itemDesc: string;
-		consumedLocation: string;
-		consumptionCompany: string;
-		feelingPrior: string;
-		feelingPost: string;
-		selfTalk: string;
-		otherComment: string;
-	};
+	//Test userId untill signUp is implmented
+	const USERID = "11111111-1111-1111-1111-111111111111";
 
-	const [journalEntrys, setJournalEntrys] = useState<JournalEntry[]>([])
+	const [journalEntrys, setJournalEntrys] = useState<Entry[]>([])
 
-	function addEntry(newEntry: JournalEntry){
-    setJournalEntrys(prevEntries => {
-      return [...prevEntries, newEntry]
-    })
-  }
+	async function fetchAllUserData(userid: string) {
+		try {
+			const result = await getAllUserEntrys(userid);
+			if (result.data.length > 0) {
+				const userData: Entry[] = result.data;
+				setJournalEntrys(userData);
+			}
+		} catch (error) {
+			console.error('API Error:', error);
+		}
+	}
 
-	function deleteEntry(id: number)
-  {
-    setJournalEntrys(prevEntrys => {
-      return prevEntrys.filter((entry, index)=>{ return index !== id;})
-    })
-  }
+	useEffect(() => {
+		fetchAllUserData(USERID);
+	}, []);
+
+	function addEntry(newEntry: Entry) {
+		setJournalEntrys(prevEntries => {
+			return [...prevEntries, newEntry]
+		})
+	}
+
+	function deleteEntry(id: number) {
+		setJournalEntrys(prevEntrys => {
+			return prevEntrys.filter((entry, index) => { return index !== id; })
+		})
+	}
 	return (
 		<div className="App">
 			<Header />
-			<NewEntryForm createEntry = {addEntry}/>
-			{journalEntrys.map((entry, index) => 
-			<JournalEntryDisplayBlock
-			key={index}
-			id={index}
-			itemDate= {entry.itemDate}
-			timeConsumed={entry.timeConsumed}
-			itemDesc={entry.itemDesc}
-			consumedLocation={entry.consumedLocation}
-			consumptionCompany={entry.consumptionCompany}
-			feelingPrior={entry.feelingPrior}
-			feelingPost={entry.feelingPost}
-			selfTalk={entry.selfTalk}
-			otherComment={entry.otherComment}
-			deleteEntry={deleteEntry} />)}
+			<NewEntryForm createEntry={addEntry} />
+			{
+				journalEntrys.map((entry, index) =>
+					<JournalEntryDisplayBlock
+						key={index}
+						id={entry.id}
+						itemDate={entry.item_date}
+						timeConsumed={entry.time_consumed}
+						itemDesc={entry.item_desc}
+						consumedLocation={entry.consumed_location}
+						consumptionCompany={entry.consumption_company}
+						feelingPrior={entry.feeling_prior}
+						feelingPost={entry.feeling_post}
+						selfTalk={entry.self_talk}
+						otherComment={entry.other_comment}
+						deleteEntry={deleteEntry} />)}
 		</div>
 	);
 }
