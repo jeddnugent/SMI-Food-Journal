15ca452f965a046851a839c951b3d6d1e05cbeb2
jeddnuget <jeddnugget@gmail.com>
2025-05-12 20:15:36 +1000

    Fixing possible HTML tree issue

--- client/src/components/JournalEntryDisplayBlock.tsx
+++ client/src/components/JournalEntryDisplayBlock.tsx
@@ -19,38 +19,40 @@ function JournalEntryDisplayBlock(props: {
 		<div className="JornalDisplay">
 			<h4>{props.itemDate} {props.timeConsumed} Entry</h4>
 			<table>
-				<tr>
-					<td>Item Descripton: </td>
-					<td>{props.itemDesc}</td>
-				</tr>
-				<tr>
-					<td>Consumption Location: </td>
-					<td>{props.consumedLocation}</td>
-				</tr>
-				<tr>
-					<td>Consumption Company: </td>
-					<td>{props.consumptionCompany}</td>
-				</tr>
-				<tr>
-					<td>Feelings Prior: </td>
-					<td>{props.feelingPrior}</td>
-				</tr>
-				<tr>
-					<td>Feelings Post: </td>
-					<td>{props.feelingPost}</td>
-				</tr>
-				<tr>
-					<td>Self Talk: </td>
-					<td>{props.selfTalk}</td>
-				</tr>
-				<tr>
-					<td>Other Comment: </td>
-					<td>{props.otherComment}</td>
-				</tr>
+				<thead>
+					<tr>
+						<td>Item Descripton: </td>
+						<td>{props.itemDesc}</td>
+					</tr>
+					<tr>
+						<td>Consumption Location: </td>
+						<td>{props.consumedLocation}</td>
+					</tr>
+					<tr>
+						<td>Consumption Company: </td>
+						<td>{props.consumptionCompany}</td>
+					</tr>
+					<tr>
+						<td>Feelings Prior: </td>
+						<td>{props.feelingPrior}</td>
+					</tr>
+					<tr>
+						<td>Feelings Post: </td>
+						<td>{props.feelingPost}</td>
+					</tr>
+					<tr>
+						<td>Self Talk: </td>
+						<td>{props.selfTalk}</td>
+					</tr>
+					<tr>
+						<td>Other Comment: </td>
+						<td>{props.otherComment}</td>
+					</tr>
+				</thead>
 			</table>
-			<button onClick={()=>{props.deleteEntry(props.id)}}>
-        <DeleteIcon />
-      </button>
+			<button onClick={() => { props.deleteEntry(props.id) }}>
+				<DeleteIcon />
+			</button>
 		</div>
 	);
 }
