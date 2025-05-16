166514342a806a71d353358fae17a77453d49376
jeddnuget <jeddnugget@gmail.com>
2025-05-16 10:34:45 +1000

    LoginSignup cleanup

--- client/src/components/LoginSignUp.tsx
+++ client/src/components/LoginSignUp.tsx
@@ -1,46 +1,16 @@
 import { useState } from 'react';
-import { AppProvider } from '@toolpad/core/AppProvider';
-import { SignInPage, type AuthProvider } from '@toolpad/core/SignInPage';
-import { useTheme } from '@mui/material/styles';
-import { login } from "../api/users"
 import EmailIcon from '@mui/icons-material/Email';
 import HttpsIcon from '@mui/icons-material/Https';
 import PersonIcon from '@mui/icons-material/Person';
 import "../styles/LoginSignUp.css";
 
 
-
-// const BRANDING = {
-// 	logo: (
-// 		<img
-// 			src="https://successfulminds.com.au/wp-content/uploads/2019/09/web-200.png"
-// 			alt="SMI logo"
-// 			style={{ height: 100 }}
-// 		/>
-// 	),
-// 	title: 'SMI Food Journal',
-// };
-
-
-
-
-
 function LoginSignUp() {
-	const theme = useTheme();
-	const signIn: (provider: AuthProvider) => void = async (provider) => {
-		console.log(provider)
-		console.log(provider.name)
-	};
-
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
 	const [action, setAction] = useState("Sign Up")
 
-	const providers = [{ id: 'credentials', name: 'Email and Password' }];
-
-
 	return (
-		// preview-start
 		<div className='container'>
 			<div className="header">
 				<div className="text">{action}</div>
@@ -70,6 +40,5 @@ function LoginSignUp() {
 		</div>
 	);
 }
-
 
 export default LoginSignUp;
