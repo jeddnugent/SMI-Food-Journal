88d35ec9590081a86fd871589ee67d3a852ec5b9
jeddnuget <jeddnugget@gmail.com>
2025-05-28 15:40:45 +1000

    Implementing Login and Logout routesm
    Also modifying axois calls to provide user calls with the ability to pass creds with a axios instance

--- client/src/api/users.ts
+++ client/src/api/users.ts
@@ -1,21 +1,6 @@
-import axios from "axios";
-import type { User } from "../interfaces/User";
-import type { Credentials } from "../interfaces/Credentials";
-const API_URL = import.meta.env.VITE_API_URL;
+import axiosInstance from "./axiosInstance";
 
-export const login = (data: Credentials) =>
-  axios.post(`${API_URL}/login`, data);
+export const login = (username: string, password: string) =>
+  axiosInstance.post(`/user/login`, { username, password });
 
-
-export const loginUser = (data: Credentials) =>
-  axios.post(
-    `${API_URL}/login`,
-    data,
-    {
-      withCredentials: true, // 🔑 allow sending/receiving cookies
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    }
-  );
-
+export const logout = () => axiosInstance.post(`/user/logout`);
