195937505f2ebcbbe7a96b3b635c9494d9f25407
jeddnuget <jeddnugget@gmail.com>
2025-05-30 13:47:17 +1000

    Adding CheckAuth axios route
    route returns a user object if the user is authenticated

--- client/src/api/users.ts
+++ client/src/api/users.ts
@@ -4,3 +4,5 @@ export const login = (username: string, password: string) =>
   axiosInstance.post(`/user/login`, { username, password });
 
 export const logout = () => axiosInstance.post(`/user/logout`);
+
+export const checkAuth = () => axiosInstance.get(`/user/check-auth`);
