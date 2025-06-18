cfc1e715245e3064d0afaa59746908ee64f33a29
jeddnuget <jeddnugget@gmail.com>
2025-06-18 22:30:23 +1000

    Fixing main.tsx and index.html
    Further tweaking for railway pipeline, more likely to come

--- client/src/Pages/main.tsx
+++ client/src/Pages/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import App from "./App";
-import DashboardLayoutWrapper from './Pages/DashboardLayout';
-import CreateEntrys from './Pages/CreateEntrys';
-import JournalOverview from './Pages/JournalOverview';
-import LoginSignUp from './Pages/LoginSignUp';
-import { UserProvider } from './contexts/UserContext';
-import ProtectedRoute from './components/ProtectedRoute';
+import App from "../App";
+import DashboardLayoutWrapper from './DashboardLayout';
+import CreateEntrys from './CreateEntrys';
+import JournalOverview from './JournalOverview';
+import LoginSignUp from './LoginSignUp';
+import { UserProvider } from '../contexts/UserContext';
+import ProtectedRoute from '../components/ProtectedRoute';
 
 
 
