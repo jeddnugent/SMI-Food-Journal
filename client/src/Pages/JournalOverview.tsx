35dfb754eeb307dfa7692440892b77a8fb07ade1
jeddnuget <jeddnugget@gmail.com>
2025-07-08 12:10:49 +1000

    Adding Journal Overview table titles
    + adding new placeholder to journal overview screen

--- client/src/Pages/JournalOverview.tsx
+++ client/src/Pages/JournalOverview.tsx
@@ -36,6 +36,16 @@ function JournalOverview() {
 	return (
 		<div>
 			<h1>Journal Overview</h1>
+			<div className="EntryHeading-Container">
+				<h4>Food / Drink</h4>
+				<h4>Time</h4>
+				<h4>Date</h4>
+				<h4>Location</h4>
+				<h4>Company</h4>
+				<h4>Feelings Prior</h4>
+				<h4>Feelings Post</h4>
+				<h4>Self Talk</h4>
+			</div>
 			<ul className='EntryList'>
 				{Array.isArray(entries)
 					? entries.map((jorunalEntry: Entry, index) => (
@@ -48,6 +58,8 @@ function JournalOverview() {
 					))
 					: <p>No entries found or loading...</p>
 				}
+						: <div className='Placeholder'>Today's entries are still waiting to be written.</div>
+					}
 			</ul>
 		</div>
 	);
